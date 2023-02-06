927739fc0f5ce1dc259344a4bc9bf769150487b6
lucopdev <lucopdev@gmail.com>
2023-02-06 17:46:58 -0300

    feature get Pokemon

--- src/main.js
+++ src/main.js
@@ -1,16 +1,17 @@
-import Pokedex from 'pokedex-promise-v2';
-const options = {
-  versionPath: '/api/v2/',
-  cacheLimit: 100 * 1000, // 100s
-  timeout: 5 * 1000 // 5s
-}
-const pokedex = new Pokedex(options);
+const searchPokemon = document.getElementById('searchPokemon');
+const searchButtom = document.getElementById('searchButton');
 
-
-pokedex.getPokemonByName("pikachu")
-.then((response) => {
-  console.log(response);
-})
-.catch((error) => {
-  console.log('There was an ERROR: ', error);
+searchButtom.addEventListener('click', () => {
+  let url = `https://pokeapi.co/api/v2/pokemon/${searchPokemon.value}`;
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      document.getElementById('name').innerHTML = data.name;
+    })
+    .catch((erro) => {
+      console.log('erro' + erro)
+    });
 });
