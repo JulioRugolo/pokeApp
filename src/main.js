93e4b517ef763ba7509f1536acea2ac84c4453d0
JulioRugolo <juliorugolo@gmail.com>
2023-02-06 17:51:37 -0300

    testando

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-const searchPokemon = document.getElementById('searchPokemon');
+const input = document.getElementById('searchPokemon');
 const searchButtom = document.getElementById('searchButton');
 
 searchButtom.addEventListener('click', () => {
