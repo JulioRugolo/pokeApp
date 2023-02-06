191e000d39756360278149af70696712bae7d2f8
lucopdev <lucopdev@gmail.com>
2023-02-06 17:36:18 -0300

    feature get Pokemon

--- src/main.js
+++ src/main.js
@@ -1,18 +1,16 @@
+const searchPokemon = document.getElementById('searchPokemon');
+const searchButtom = document.getElementById('searchButton');
 
-const poke = 'pikachu';
-
-const loadPokemon = () => {
-  let url = `https://pokeapi.co/api/v2/pokemon/${poke}`;
+searchButtom.addEventListener('click', () => {
+  let url = `https://pokeapi.co/api/v2/pokemon/${searchPokemon.value}`;
   fetch(url)
     .then((response) => {
       return response.json();
     })
     .then((data) => {
-      console.clear();
-      document.getElementById('text-name').innerHTML = data.name;
+      document.getElementById('name').innerHTML = data.name;
     })
     .catch((erro) => {
       console.log('erro' + erro)
     });
-};
-loadPokemon();
+});
